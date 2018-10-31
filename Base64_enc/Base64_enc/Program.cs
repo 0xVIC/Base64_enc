1600aec9407c148d054fabade4fde6764dc517c9
VICTOR <26430812+0xVIC@users.noreply.github.com>
2018-10-31 15:49:21 +0100

    Add files

---
+++ Base64_enc/Base64_enc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+public class Base64_enc
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("EXAMPLE: base64_enc.exe [path_to_file]");
+            return;
+        }
+
+        using (FileStream fs = File.OpenRead(args[0]))
+        {
+            byte[] data = new byte[fs.Length];
+            fs.Read(data, 0, (int)fs.Length);
+            Console.WriteLine(Convert.ToBase64String(data));
+        }
+    }
+}
